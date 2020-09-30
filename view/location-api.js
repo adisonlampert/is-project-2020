4475770ce326695b30ddca8e5c937e282e8b4f40
Adison Lampert <38566184+addybug@users.noreply.github.com>
2020-09-30 10:57:24 -0500

    delete unused files

--- view/location-api.js
+++
@@ -1,8 +0,0 @@
-var autocomplete;
-function initialize() {
- autocomplete = new google.maps.places.Autocomplete(
-     /** @type {HTMLInputElement} */(document.getElementById('location')),
-     { types: ['geocode'] });
- google.maps.event.addListener(autocomplete, 'place_changed', function() {
- });
-}
