9701bb8caa93df3327987b9645397c34110062c9
Adison Lampert <38566184+addybug@users.noreply.github.com>
2020-10-12 11:08:47 -0500

    created pending list

--- view/client.js
+++ view/client.js
@@ -1,4 +1,5 @@
 var oppsList = [];
+var pendingList = [];
 
 fetch("/getOpps", {})
   .then(res => res.json())
@@ -78,6 +79,30 @@ function getValues() {
   $("#upload-formid")[0].reset();
 
 }
+
+fetch("/getPending", {})
+  .then(res => res.json())
+  .then(response => {
+    response.forEach(row => {
+      pendingList.push({
+        name: row.name,
+        category: row.category,
+        type: row.type,
+        country: row.country,
+        state: row.state,
+        city: row.city,
+        startDate: row.startDate,
+        endDate: row.endDate,
+        deadline: row.deadline,
+        cost: row.cost,
+        description: row.description,
+        link: row.link,
+        visible: true
+      });
+      console.log("Pending db");
+      console.log(pendingList);
+    });
+  });
 
 fetch("/addOpps", {
   method: "POST",
