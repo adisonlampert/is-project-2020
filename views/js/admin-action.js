062ca47aaacfdf26477cd7a1dbe2c149c258eb96
Adison Lampert <38566184+addybug@users.noreply.github.com>
2020-12-05 10:33:42 -0600

    added move onclick function

--- views/js/admin-action.js
+++ views/js/admin-action.js
@@ -44,9 +44,39 @@ $("#update").on('click', function() {
     id: $("#id").val(),
 		visibility: $("#visibility").val(),
 	}
-  console.log(createChange);
 
   fetch("/updatePending", {
+    method: 'POST',
+    body: JSON.stringify(createChange),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  location.reload();
+  event.stopPropagation();
+});
+
+$("#move").on('click', function() {
+
+  var createChange = {
+		name: $("#name").val(),
+		category: $("#category").val(),
+		type: $("#type").val(),
+		country: $("#country").val(),
+		state: $("#state").val(),
+		city: $("#city").val(),
+		startDate: $("#startDate").val(),
+		endDate: $("#endDate").val(),
+		deadline: $("#deadline").val(),
+		cost: $("#cost").val(),
+		currency: $("#currency").val(),
+		description: $("#description").val(),
+		link: $("#link").val(),
+    id: $("#id").val(),
+		visibility: $("#visibility").val(),
+	}
+
+  fetch("/movePending", {
     method: 'POST',
     body: JSON.stringify(createChange),
     headers: {
