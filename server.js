91b1f08079c2c004283b65562e0297feb1ed8de1
Adison Lampert <38566184+addybug@users.noreply.github.com>
2020-12-06 21:15:05 -0600

    added logo to navbar

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ con.connect(function(err) {
   // });
 });
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
